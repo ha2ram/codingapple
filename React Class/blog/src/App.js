c69282b12598aa91782094331f63f4901395c921
Haram Jeong <niceharam@gmail.com>
2021-12-06 20:45:35 +0900

    state 변경

--- React Class/blog/src/App.js
+++ React Class/blog/src/App.js
@@ -1,9 +1,17 @@
+/* eslint-disable */
+
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
+  let [글제목, 글제목변경] = useState([
+    '남자 코트 추천',
+    '강남 우동 맛집',
+    '파이썬독학',
+  ]);
+
+  let [따봉, 따봉변경] = useState(0);
 
   let posts = '강남 고기 맛집';
 
@@ -13,8 +21,28 @@ function App() {
         <div style={{ color: 'blue', fontSize: '30px' }}>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> {글제목[0]} </h3>
+        <h3>
+          {글제목[0]}{' '}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{' '}
+          {따봉}
+        </h3>
         <p>2월 17일 발행</p>
+        <hr></hr>
+      </div>
+      <div className="list">
+        <h3> {글제목[1]} </h3>
+        <p>2월 18일 발행</p>
+        <hr></hr>
+      </div>
+      <div className="list">
+        <h3> {글제목[2]} </h3>
+        <p>2월 19일 발행</p>
         <hr></hr>
       </div>
     </div>
