1a84ed0783650427c75d4e6c9588116d90cc27d3
Haram Jeong <niceharam@gmail.com>
2021-12-28 18:04:19 +0900

    dataset

--- JavaScript/6.js
+++ JavaScript/6.js
@@ -1,12 +1,32 @@
-for (let i = 0; i < $('.tab-button').length; i++) {
-  $('.tab-button')
-    .eq(i)
-    .click(function () {
-      $('.tab-button').removeClass('active');
-      $('.tab-content').removeClass('show');
-      $('.tab-button').eq(i).addClass('active');
-      $('.tab-content').eq(i).addClass('show');
-    });
+$('.list').click(function (e) {
+  탭열기(e.target.dataset.id);
+});
+
+// $('.list').click(function (e) {
+//   if (e.target == document.querySelectorAll('.tab-button')[0]) {
+//     탭열기(0);
+//   }
+//   if (e.target == document.querySelectorAll('.tab-button')[1]) {
+//     탭열기(1);
+//   }
+//   if (e.target == document.querySelectorAll('.tab-button')[2]) {
+//     탭열기(2);
+//   }
+// });
+
+// for (let i = 0; i < $('.tab-button').length; i++) {
+//   $('.tab-button')
+//     .eq(i)
+//     .click(function () {
+//       탭열기(i);
+//     });
+// }
+
+function 탭열기(숫자) {
+  $('.tab-button').removeClass('active');
+  $('.tab-content').removeClass('show');
+  $('.tab-button').eq(숫자).addClass('active');
+  $('.tab-content').eq(숫자).addClass('show');
 }
 
 // $('.tab-button')
