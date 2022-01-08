3f81638eb16f8a1c56acfe7327e56e6ac0cee4a4
Haram Jeong <niceharam@gmail.com>
2022-01-08 23:51:55 +0900

    array state 변경

--- React Class/blog/src/App.js
+++ React Class/blog/src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 
 function App() {
   let [글제목, 글제목변경] = useState([
-    '남자 코트 추천',
-    '강남 우동 맛집',
+    '남자코트 추천',
+    '강남 우동맛집',
     '파이썬독학',
   ]);
 
@@ -15,21 +15,28 @@ function App() {
 
   let posts = '강남 고기 맛집';
 
+  function 제목바꾸기() {
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div style={{ color: 'blue', fontSize: '30px' }}>개발 Blog</div>
       </div>
+      <button onClick={제목바꾸기}>버튼</button>
       <div className="list">
         <h3>
-          {글제목[0]}{' '}
+          {글제목[0]}
           <span
             onClick={() => {
               따봉변경(따봉 + 1);
             }}
           >
             👍
-          </span>{' '}
+          </span>
           {따봉}
         </h3>
         <p>2월 17일 발행</p>
