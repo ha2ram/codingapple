6095f47b7bcc9ee581c1463167325b84885b7d25
Haram Jeong <niceharam@gmail.com>
2022-01-11 17:11:42 +0900

    component

--- React Class/blog/src/App.js
+++ React Class/blog/src/App.js
@@ -52,6 +52,18 @@ function App() {
         <p>2월 19일 발행</p>
         <hr></hr>
       </div>
+
+      <Modal />
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
