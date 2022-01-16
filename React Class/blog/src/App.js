1b5b87dd821aa5a0ccae42714583f26098596355
Haram Jeong <niceharam@gmail.com>
2022-01-16 19:35:11 +0900

    props 응용

--- React Class/blog/src/App.js
+++ React Class/blog/src/App.js
@@ -10,10 +10,9 @@ function App() {
     '강남 우동맛집',
     '파이썬독학',
   ]);
-
   let [따봉, 따봉변경] = useState(0);
-
   let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
 
   var 어레이 = [2, 3, 4];
 
@@ -45,7 +44,7 @@ function App() {
         <div style={{ color: 'blue', fontSize: '30px' }}>개발 Blog</div>
       </div>
       <button onClick={제목바꾸기}>버튼</button>
-      <div className="list">
+      {/* <div className="list">
         <h3>
           {글제목[0]}
           <span
@@ -69,14 +68,18 @@ function App() {
         <h3>{글제목[2]}</h3>
         <p>2월 19일 발행</p>
         <hr></hr>
-      </div>
+      </div> */}
 
       {/* {반복된UI()} */}
 
-      {글제목.map(function (글) {
+      {글제목.map(function (글, i) {
         return (
           <div className="list">
-            <h3>
+            <h3
+              onClick={() => {
+                누른제목변경(i);
+              }}
+            >
               {글}
               <span
                 onClick={() => {
@@ -93,9 +96,31 @@ function App() {
         );
       })}
 
+      <button
+        onClick={() => {
+          누른제목변경(0);
+        }}
+      >
+        버튼1
+      </button>
+      <button
+        onClick={() => {
+          누른제목변경(1);
+        }}
+      >
+        버튼2
+      </button>
+      <button
+        onClick={() => {
+          누른제목변경(2);
+        }}
+      >
+        버튼3
+      </button>
+
       <button onClick={() => modal변경(!modal)}>열고닫기</button>
 
-      {modal === true ? <Modal 글제목={글제목} /> : null}
+      {modal === true ? <Modal 글제목={글제목} 누른제목={누른제목} /> : null}
     </div>
   );
 }
@@ -103,7 +128,7 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h2> {props.글제목[1]} </h2>
+      <h2> {props.글제목[props.누른제목]} </h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
