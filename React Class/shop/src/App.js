93385b452cbb1f815d03e6ba9d6fe7527c4e6d94
Haram Jeong <niceharam@gmail.com>
2022-01-17 23:48:45 +0900

    react-router-dom

--- React Class/shop/src/App.js
+++ React Class/shop/src/App.js
@@ -5,6 +5,8 @@ import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
 
+import { Link, Route, Switch } from 'react-router-dom';
+
 function App() {
   let [shoes, shoes변경] = useState(Data);
 
@@ -36,24 +38,45 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="background">
-        <h1>20% Season Off</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eius,
-          dignissimos, rerum, quas voluptatum expedita culpa blanditiis deleniti
-          voluptates illum praesentium. Molestias accusantium eius odit culpa
-          voluptates nesciunt sapiente adipisci.
-        </p>
-        <Button variant="primary">Learn more</Button>
-      </div>
-
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Card shoes={shoes[i]} i={i} key={i} />;
-          })}
+      <Route exact path="/">
+        <div className="background">
+          <h1>20% Season Off</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            eius, dignissimos, rerum, quas voluptatum expedita culpa blanditiis
+            deleniti voluptates illum praesentium. Molestias accusantium eius
+            odit culpa voluptates nesciunt sapiente adipisci.
+          </p>
+          <Button variant="primary">Learn more</Button>
         </div>
-      </div>
+
+        <div className="container">
+          <div className="row">
+            {shoes.map((a, i) => {
+              return <Card shoes={shoes[i]} i={i} key={i} />;
+            })}
+          </div>
+        </div>
+      </Route>
+
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://codingapple1.github.io/shop/shoes1.jpg"
+                width="100%"
+              />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
+          </div>
+        </div>
+      </Route>
     </div>
   );
 }
