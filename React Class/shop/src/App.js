2431dba90d31fc04883ccccca493327a5338b260
Haram Jeong <niceharam@gmail.com>
2022-01-18 00:56:14 +0900

    Link, Switch, history

--- React Class/shop/src/App.js
+++ React Class/shop/src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
+import Detail from './Detail.js';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -18,8 +19,16 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link>
+                <Link to="/" className="link">
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/detail" className="link">
+                  Detail
+                </Link>
+              </Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -38,45 +47,36 @@ function App() {
         </Container>
       </Navbar>
 
-      <Route exact path="/">
-        <div className="background">
-          <h1>20% Season Off</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            eius, dignissimos, rerum, quas voluptatum expedita culpa blanditiis
-            deleniti voluptates illum praesentium. Molestias accusantium eius
-            odit culpa voluptates nesciunt sapiente adipisci.
-          </p>
-          <Button variant="primary">Learn more</Button>
-        </div>
-
-        <div className="container">
-          <div className="row">
-            {shoes.map((a, i) => {
-              return <Card shoes={shoes[i]} i={i} key={i} />;
-            })}
+      <Switch>
+        <Route exact path="/">
+          <div className="background">
+            <h1>20% Season Off</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              eius, dignissimos, rerum, quas voluptatum expedita culpa
+              blanditiis deleniti voluptates illum praesentium. Molestias
+              accusantium eius odit culpa voluptates nesciunt sapiente adipisci.
+            </p>
+            <Button variant="primary">Learn more</Button>
           </div>
-        </div>
-      </Route>
 
-      <Route path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img
-                src="https://codingapple1.github.io/shop/shoes1.jpg"
-                width="100%"
-              />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button>
+          <div className="container">
+            <div className="row">
+              {shoes.map((a, i) => {
+                return <Card shoes={shoes[i]} i={i} key={i} />;
+              })}
             </div>
           </div>
-        </div>
-      </Route>
+        </Route>
+
+        <Route path="/detail">
+          <Detail />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나 적었을 때 이거 보여주셈</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
