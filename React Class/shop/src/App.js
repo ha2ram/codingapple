cca6797c724e12e32e9067bdd4239c21f9d851c2
Haram Jeong <niceharam@gmail.com>
2022-01-18 13:26:46 +0900

    useParams

--- React Class/shop/src/App.js
+++ React Class/shop/src/App.js
@@ -69,8 +69,8 @@ function App() {
           </div>
         </Route>
 
-        <Route path="/detail">
-          <Detail />
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} />
         </Route>
 
         <Route path="/:id">
--- React Class/shop/src/Detail.js
+++ React Class/shop/src/Detail.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
-function Detail() {
+function Detail(props) {
+  let { id } = useParams();
   let history = useHistory();
+  let 찾은상품 = props.shoes.find(function (상품) {
+    return 상품.id == id;
+  });
 
   return (
     <div className="container">
@@ -15,9 +19,9 @@ function Detail() {
           />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
           <button className="btn btn-danger">주문하기</button>
           <button
             className="btn btn-danger"
