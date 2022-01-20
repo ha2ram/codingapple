4ac2bb3c20514c5577deec5485c3bd0508640eee
Haram Jeong <niceharam@gmail.com>
2022-01-20 20:32:07 +0900

    react-transition-group

--- React Class/shop/src/Detail.js
+++ React Class/shop/src/Detail.js
@@ -3,6 +3,9 @@ import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
 import { 재고context } from './App.js';
+import { Nav } from 'react-bootstrap';
+
+import { CSSTransition } from 'react-transition-group';
 
 let 박스 = styled.div`
   padding: 20px;
@@ -25,6 +28,9 @@ function Detail(props) {
   let [alert, alert변경] = useState(true);
   let [inputData, inputData변경] = useState('');
   let 재고 = useContext(재고context);
+
+  let [누른탭, 누른탭변경] = useState(0);
+  let [스위치, 스위치변경] = useState(false);
 
   useEffect(() => {
     let 타이머 = setTimeout(() => {
@@ -93,8 +99,62 @@ function Detail(props) {
           </button>
         </div>
       </div>
+
+      <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-0"
+            onClick={() => {
+              스위치변경(false);
+              누른탭변경(0);
+            }}
+          >
+            Active
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-1"
+            onClick={() => {
+              스위치변경(false);
+              누른탭변경(1);
+            }}
+          >
+            Option 1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-2"
+            onClick={() => {
+              스위치변경(false);
+              누른탭변경(2);
+            }}
+          >
+            Option 2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <CSSTransition in={스위치} classNames="wow" timeout={500}>
+        <TabContent 누른탭={누른탭} 스위치변경={스위치변경} />
+      </CSSTransition>
     </div>
   );
+}
+
+function TabContent(props) {
+  useEffect(() => {
+    props.스위치변경(true);
+  });
+
+  if (props.누른탭 === 0) {
+    return <div>0번째 내용입니다</div>;
+  } else if (props.누른탭 === 1) {
+    return <div>1번째 내용입니다</div>;
+  } else if (props.누른탭 === 2) {
+    return <div>2번째 내용입니다</div>;
+  }
 }
 
 function Info(props) {
