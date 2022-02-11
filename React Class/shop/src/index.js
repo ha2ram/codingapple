d3bb4ee674407c19628166ab1f0136b778d989ee
Haram Jeong <niceharam@gmail.com>
2022-02-11 23:20:27 +0900

    reducer2

--- React Class/shop/src/index.js
+++ React Class/shop/src/index.js
@@ -9,7 +9,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+
+let alert초기값 = true;
+
+function reducer2(state = alert초기값, 액션) {
+  if (액션.type === 'alert닫기') {
+    state = false;
+    return state;
+  } else {
+    return state;
+  }
+}
 
 let 초기값 = [
   { id: 0, name: '멋진신발', quan: 2 },
@@ -30,7 +41,7 @@ function reducer(state = 초기값, 액션) {
   }
 }
 
-let store = createStore(reducer);
+let store = createStore(combineReducers({ reducer, reducer2 }));
 
 ReactDOM.render(
   <React.StrictMode>
